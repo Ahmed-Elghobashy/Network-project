f206ec824a98a2243baf768c08f350831c7197ec
Ahmed Samir El Ghobashy <ahmed@Ahmeds-MacBook-Pro.local>
2020-11-16 00:09:06 +0200

    fixed error

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -6,21 +6,24 @@ function loginController(req, res) {
     console.log(req.body);
     const credentials = req.body;
     if (databaseContainsUsername(credentials.username)) {
-        res.send("fuck you");
+        res.send("thank you");
     }
     else {
         //redirect the user to the main page
-        res.send("thank you")
+        res.send("fuck you");
     }
 }
 
 function databaseContainsUsername(username) {
 
-    const data = fs.readFileSync(path.join('users.json'));
-    const users = JSON.parse(data);
-    for (const user in users) {
-        if (user.username === username)
+    const data = (fs.readFileSync(path.join('users.json')));
+    const users = JSON.parse(data).users;
+    console.log(users);
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
             return true;
+
+        }
     }
     return false;
 }
